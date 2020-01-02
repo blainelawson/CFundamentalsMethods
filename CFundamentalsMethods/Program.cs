4f08d51129bc8e7c5c597f32d80952dc381a400f
Blaine M Lawson <blaine.lawson@gmail.com>
2020-01-02 16:38:36 -0500

    First Commit

---
+++ CFundamentalsMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CFundamentalsMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Positive or Negative?");
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int.TryParse(input, out int num);
+            Console.WriteLine($"The number {num} is {PositiveOrNegative(num)}");
+        }
+
+        public static string PositiveOrNegative(int num)
+        {
+              if (num > 0)
+            {
+                return "positive";
+            }
+              else if (num == 0)
+            {
+                return "neither positive nor negative";
+            }
+              else
+            {
+                return "negative";
+            }
+        }
+    }
+}
